d181bdbb19de2e7a38c3110161040df1efe39109
yoshinari0508 <yoshinari0508@icloud.com>
2024-05-10 19:51:44 +0900

    未実装

--- code/.vscode/selection100/bfs/joi2012yo_e.cpp
+++ code/.vscode/selection100/bfs/joi2012yo_e.cpp
@@ -11,75 +11,74 @@ const int di[] = {1, 0, -1, 0};
 const int dj[] = {0, 1, 0, -1};
 const double PI = acos(-1);
 
-const int di6_odd[] = {-1, 0, 0, 1, 1, 1};
-const int dj6_odd[] = {0, -1,  1, -1, 0, 1};
-const int di6_even[] = {-1, -1, -1, 0, 1, 0};
-const int dj6_even[] = {0, -1, 1, -1, 0, 1};
+const int di6_odd[] = {1, 0, -1, 0, 1, -1};
+const int dj6_odd[] = {0, 1, 0, -1, 1, 1};
+const int di6_even[] = {1, 0, -1, 0, 1, -1};
+const int dj6_even[] = {0, 1, 0, -1, -1, -1};
 
 int main(){
     int h, w;
     cin >> h >> w;
     
-    char s[w+1][h+1];
+    vector<vector<char>> s(w+2, vector<char>(h+2, 0));
     rep2(i, 1, w){
         rep2(j, 1, h) cin >> s[i+1][j+1];
     }
 
-    bool visited[w+1][h+1];
+    vector<vector<bool>> visited(w+2, vector<bool>(h+2, false));
     queue<pair<int, int> > q;
-
 
     int cnt = 0;
     rep2(i, 1, w){
         rep2(j, 1, h){
-            if(i != 1 && i != w && j != 1 && j != h) continue;
-            if(s[i][j] == '1'){
-                if(j%2 == 1){
-                    rep(v, 6){
-                        int ni = i + di6_even[v];
-                        int nj = j + dj6_even[v];
-                        if(ni <= 0 || ni > w || nj <= 0 || nj > h) cnt++;
-                    }
-                }else{
-                    rep(v, 6){
-                        int ni = i + di6_odd[v];
-                        int nj = j + dj6_odd[v];
-                        if(ni <= 0 || ni > w || nj <= 0 || nj > h) cnt++;
-                    }
-                }
-            }else if(s[i][j] == '0'){
-                if(visited[i][j] == true) continue;
-                visited[i][j] = true;
-                q.emplace(i, j);
-                if(j%2 == 1){
-                    while(!q.empty()){
-                        auto [ii, jj] = q.front(); q.pop();
-                        visited[ii][jj] = true; 
+            //端の部分のみ探索
+            if(i == 1 || i == w || j == 1 || j == h){
+                //建物である時
+                if(s[i][j] == '1'){
+                    if(j%2 == 1){
                         rep(v, 6){
-                        int ni = ii + di6_even[v];
-                        int nj = jj + dj6_even[v];
-                        if(ni <= 0 || ni > w || nj <= 0 || nj > h) continue;
-                        if(visited[ni][nj] == true) continue;
-                        if(s[ni][nj] == '0') q.emplace(ni, nj);
-                        if(s[ni][nj] == '1') cnt++;
+                            int ni = i + di6_odd[v];
+                            int nj = j + dj6_odd[v];
+                            if(ni <= 0 || ni > w || nj <= 0 || nj > h) cnt++;
+                        }
+                    }else{
+                        rep(v, 6){
+                            int ni = i + di6_even[v];
+                            int nj = j + dj6_even[v];
+                            if(ni <= 0 || ni > w || nj <= 0 || nj > h) cnt++;
                         }
                     }
-                }else{
+                //空間である時
+                }else if(s[i][j] == '0'){
+                    if(visited[i][j] == true) continue;
+                    visited[i][j] = true;
+                    q.emplace(i, j);
                     while(!q.empty()){
                         auto [ii, jj] = q.front(); q.pop();
-                        visited[ii][jj] = true; 
-                        rep(v, 6){
-                        int ni = ii + di6_odd[v];
-                        int nj = jj + dj6_odd[v];
-                        if(ni <= 0 || ni > w || nj <= 0 || nj > h) continue;
-                        if(visited[ni][nj] == true) continue;
-                        if(s[ni][nj] == '0') q.emplace(ni, nj);
-                        if(s[ni][nj] == '1') cnt++;
+                        visited[ii][jj] = true;
+                        if(j%2 == 1){
+                            rep(v, 6){
+                                int ni = ii + di6_odd[v];
+                                int nj = jj + dj6_odd[v];
+                                if(ni <= 0 || ni > w || nj <= 0 || nj > h) continue;
+                                if(visited[ni][nj] == true) continue;
+                                if(s[ni][nj] == '0') q.emplace(ni, nj);
+                                if(s[ni][nj] == '1') cnt++;
+                            }
+                        }else{
+                            rep(v, 6){
+                                int ni = ii + di6_even[v];
+                                int nj = jj + dj6_even[v];
+                                if(ni <= 0 || ni > w || nj <= 0 || nj > h) continue;
+                                if(visited[ni][nj] == true) continue;
+                                if(s[ni][nj] == '0') q.emplace(ni, nj);
+                                if(s[ni][nj] == '1') cnt++;
+                            }
                         }
                     }
                 }
             }
-            cout << i << " " << j << endl;
+            cout << i << ' ' << j << endl;
             cout << cnt << endl;
         }
     }
