907fa52e877d4ef069f45fb58334230d3f3dd69c
yoshinari0508 <yoshinari0508@icloud.com>
2024-06-27 00:01:36 +0900

    day23

---
+++ code/.vscode/typical90/052/typical90_az.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(p) (p).begin(), (p).end()
+#define exists(c, e) ((c).find(e) != (c).end())
+
+struct INIT{
+ INIT(){
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  cout << fixed << setprecision(20);
+ }
+}INIT;
+
+const ll MOD = 1000000007;
+int main() {
+    ll n;
+    cin >> n;
+    vector<vector<ll>> a(n, vector<ll> (6));
+    rep(i, n) rep(j, 6) cin >> a[i][j];
+
+    vector<ll> s(n, 0);
+    ll ans = 1;
+    rep(i, n){
+        rep(j, 6) s[i] += a[i][j];
+    }
+    rep(i, n){
+        ans *= s[i];
+        ans %= MOD;
+    }
+
+    cout << ans << endl;
+    return 0;
+
+}
