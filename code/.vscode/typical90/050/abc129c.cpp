d608fbd9516225f1de3fc17dfe3e8d32d1445130
yoshinari0508 <yoshinari0508@icloud.com>
2024-06-27 00:00:33 +0900

    day22

---
+++ code/.vscode/typical90/050/abc129c.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(p) (p).begin(), (p).end()
+#define exists(c, e) ((c).find(e) != (c).end())
+
+struct INIT{
+ INIT(){
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  cout << fixed << setprecision(20);
+ }
+}INIT;
+
+template <std::uint_fast64_t Modulus> class modint {
+	using u64 = std::uint_fast64_t;
+public:
+	u64 a;
+	constexpr modint(const u64 x = 0) noexcept : a(x % Modulus) {}
+	constexpr u64 &value() noexcept { return a; }
+	constexpr const u64 &value() const noexcept { return a; }
+	constexpr modint operator+(const modint rhs) const noexcept {
+		return modint(*this) += rhs;
+	}
+	constexpr modint operator-(const modint rhs) const noexcept {
+		return modint(*this) -= rhs;
+	}
+	constexpr modint operator*(const modint rhs) const noexcept {
+    	return modint(*this) *= rhs;
+	}
+	constexpr modint operator/(const modint rhs) const noexcept {
+		return modint(*this) /= rhs;
+	}
+	constexpr modint &operator+=(const modint rhs) noexcept {
+		a += rhs.a;
+		if (a >= Modulus) {
+			a -= Modulus;
+		}
+    	return *this;
+	}
+	constexpr modint &operator-=(const modint rhs) noexcept {
+		if (a < rhs.a) {
+			a += Modulus;
+		}
+		a -= rhs.a;
+		return *this;
+	}
+	constexpr modint &operator*=(const modint rhs) noexcept {
+    	a = a * rhs.a % Modulus;
+    	return *this;
+	}
+	constexpr modint &operator/=(modint rhs) noexcept {
+		u64 exp = Modulus - 2;
+		while (exp) {
+			if (exp % 2) {
+				*this *= rhs;
+			}
+			rhs *= rhs;
+			exp /= 2;
+		}
+    	return *this;
+	}
+
+    friend std::ostream& operator<<(std::ostream& os, const modint& rhs) {
+        os << rhs.a;
+        return os;
+    }
+};
+
+using mint = modint<1000000007>;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<bool> b(1e5+9);
+    rep(i, m){
+        int tmp; cin >> tmp;
+        b[tmp] = true;
+    }
+
+    vector<mint> dp(1e5+9, 0);
+    dp[0] = 1;
+    rep(i, 0, n+1){
+        rep(d, 1, 2+1){
+            if(!b[i+d]) dp[i+d] += dp[i];
+        }
+    }
+    cout << dp[n] << endl;
+    return 0;
+}
