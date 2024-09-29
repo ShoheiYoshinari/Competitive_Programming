9fa5aa905d517e1795d73c189c8ac65a5acbbcc3
yoshinari0508 <yoshinari0508@icloud.com>
2024-09-30 00:50:03 +0900

    ローカルの変更をコミット

--- code/.vscode/practice/a.cpp
+++ code/.vscode/practice/a.cpp
@@ -3,14 +3,16 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 const double pi = acos(-1);
-template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
-template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
 #define OVERLOAD_REP(_1, _2, _3, name, ...) name
 #define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
 #define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
 #define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
 #define all(p) (p).begin(), (p).end()
 #define exists(c, e) ((c).find(e) != (c).end())
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+const int dy[] = {1, 0, -1, 0};
+const int dx[] = {0, 1, 0, -1};
 
 struct INIT{
  INIT(){
@@ -19,88 +21,26 @@ struct INIT{
   cout << fixed << setprecision(20);
  }
 }INIT;
-
-//modint copy
-template <std::uint_fast64_t Modulus> class modint {
-	using u64 = std::uint_fast64_t;
-public:
-	u64 a;
-	constexpr modint(const u64 x = 0) noexcept : a(x % Modulus) {}
-	constexpr u64 &value() noexcept { return a; }
-	constexpr const u64 &value() const noexcept { return a; }
-	constexpr modint operator+(const modint rhs) const noexcept {
-		return modint(*this) += rhs;
-	}
-	constexpr modint operator-(const modint rhs) const noexcept {
-		return modint(*this) -= rhs;
-	}
-	constexpr modint operator*(const modint rhs) const noexcept {
-    	return modint(*this) *= rhs;
-	}
-	constexpr modint operator/(const modint rhs) const noexcept {
-		return modint(*this) /= rhs;
-	}
-	constexpr modint &operator+=(const modint rhs) noexcept {
-		a += rhs.a;
-		if (a >= Modulus) {
-			a -= Modulus;
-		}
-    	return *this;
-	}
-	constexpr modint &operator-=(const modint rhs) noexcept {
-		if (a < rhs.a) {
-			a += Modulus;
-		}
-		a -= rhs.a;
-		return *this;
-	}
-	constexpr modint &operator*=(const modint rhs) noexcept {
-    	a = a * rhs.a % Modulus;
-    	return *this;
-	}
-	constexpr modint &operator/=(modint rhs) noexcept {
-		u64 exp = Modulus - 2;
-		while (exp) {
-			if (exp % 2) {
-				*this *= rhs;
-			}
-			rhs *= rhs;
-			exp /= 2;
-		}
-    	return *this;
-	}
-
-    friend std::ostream& operator<<(std::ostream& os, const modint& rhs) {
-        os << rhs.a;
-        return os;
-    }
-};
-
-using mint = modint<1000000007>;
-
 int main() {
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
+    int n, x, y;
+    cin >> n >> x >> y;
+    vector<ll> a(n), b(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
 
+    sort(all(a));
+    reverse(all(a));
+    sort(all(b));
+    reverse(all(b));
+
+    ll sum_a = 0, sum_b = 0;
+    ll cnt = 0;
     rep(i, n){
-        if(i%2 == 1){
-            if(s[i] == 'A') s[i] = 'B';
-            else s[i] = 'A';
-        }
+        cnt++;
+        sum_a += a[i];
+        sum_b += b[i];
+        if(sum_a >= x || sum_b >= y) break;
     }
-    mint ans = 1;
-    int cnt = 1;
-    rep(i, n-1){
-        if(s[i] == s[i+1]) cnt++;
-        else{
-            ans *= (cnt+1)/2;
-            cnt = 1;
-        }
-    }
-    ans *= (cnt+1)/2;
-    cout << ans << endl;
+    cout << cnt << endl;
     return 0;
 }
-
