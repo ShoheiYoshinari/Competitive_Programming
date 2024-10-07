f85b8588a4261063807c2921d690ec9b067a8954
yoshinari0508 <yoshinari0508@icloud.com>
2024-10-07 21:47:49 +0900

    wa

--- code/.vscode/abc/374/e.cpp
+++ code/.vscode/abc/374/e.cpp
@@ -1,4 +1,110 @@
-#include <bits/stdc++.h>
+// C++ includes used for precompiling -*- C++ -*-
+// Copyright (C) 2003-2013 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// Under Section 7 of GPL version 3, you are granted additional
+// permissions described in the GCC Runtime Library Exception, version
+// 3.1, as published by the Free Software Foundation.
+// You should have received a copy of the GNU General Public License and
+// a copy of the GCC Runtime Library Exception along with this program;
+// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+// <http://www.gnu.org/licenses/>.
+/** @file stdc++.h
+ *  This is an implementation file for a precompiled header.
+ */
+// 17.4.1.2 Headers
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+//#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
 using namespace std;
 using ll = long long;
 
@@ -22,17 +128,21 @@ bool taskAchieve(ll W_target, vector<ll>& a, vector<ll>& p, vector<ll>& b, vecto
     ll total_cost = 0;
 
     rep(i, n) {
-        ll min_cost = LLONG_MAX;
-        for (ll s = 0; s <= W_target / a[i]; s++) {
+        ll min_cost_s = LLONG_MAX, min_cost_t = LLONG_MAX;
+        for (ll s = 0; s <= a[i]; s++) {
             ll remaining_capacity = max(0LL, W_target - s * a[i]);
-
             ll t = (remaining_capacity + b[i] - 1) / b[i];
             ll total = s * p[i] + t * q[i];
-
-            min_cost = min(min_cost, total);
+            min_cost_s = min(min_cost_s, total);
         }
-        total_cost += min_cost;
 
+        for(ll t = 0; t <= b[i]; i++){
+            ll remaining_capacity = max(0LL, W_target - t * b[i]);
+            ll s = (remaining_capacity + a[i] - 1) / a[i];
+            ll total = s * p[i] + t * q[i];
+            min_cost_t = min(min_cost_t, total);
+        }
+        total_cost = min(min_cost_s, min_cost_t);
         if (total_cost > x) return false;
     }
 
