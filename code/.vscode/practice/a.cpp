b596bbe65b8f7f92b71dd39841d52868f5aa83af
yoshinari0508 <yoshinari0508@icloud.com>
2024-10-07 21:53:10 +0900

    WA

--- code/.vscode/practice/a.cpp
+++ code/.vscode/practice/a.cpp
@@ -3,14 +3,16 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 const double pi = acos(-1);
-template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
-template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
 #define OVERLOAD_REP(_1, _2, _3, name, ...) name
 #define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
 #define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
 #define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
 #define all(p) (p).begin(), (p).end()
 #define exists(c, e) ((c).find(e) != (c).end())
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+const int dy[] = {1, 0, -1, 0};
+const int dx[] = {0, 1, 0, -1};
 
 struct INIT{
  INIT(){
@@ -20,86 +22,54 @@ struct INIT{
  }
 }INIT;
 
-//modint copy
-template <std::uint_fast64_t Modulus> class modint {
-	using u64 = std::uint_fast64_t;
-public:
-	u64 a;
-	constexpr modint(const u64 x = 0) noexcept : a(x % Modulus) {}
-	constexpr u64 &value() noexcept { return a; }
-	constexpr const u64 &value() const noexcept { return a; }
-	constexpr modint operator+(const modint rhs) const noexcept {
-		return modint(*this) += rhs;
-	}
-	constexpr modint operator-(const modint rhs) const noexcept {
-		return modint(*this) -= rhs;
-	}
-	constexpr modint operator*(const modint rhs) const noexcept {
-    	return modint(*this) *= rhs;
-	}
-	constexpr modint operator/(const modint rhs) const noexcept {
-		return modint(*this) /= rhs;
-	}
-	constexpr modint &operator+=(const modint rhs) noexcept {
-		a += rhs.a;
-		if (a >= Modulus) {
-			a -= Modulus;
-		}
-    	return *this;
-	}
-	constexpr modint &operator-=(const modint rhs) noexcept {
-		if (a < rhs.a) {
-			a += Modulus;
-		}
-		a -= rhs.a;
-		return *this;
-	}
-	constexpr modint &operator*=(const modint rhs) noexcept {
-    	a = a * rhs.a % Modulus;
-    	return *this;
-	}
-	constexpr modint &operator/=(modint rhs) noexcept {
-		u64 exp = Modulus - 2;
-		while (exp) {
-			if (exp % 2) {
-				*this *= rhs;
-			}
-			rhs *= rhs;
-			exp /= 2;
-		}
-    	return *this;
-	}
+ll n, x;
+bool taskAchieve(ll W_target, vector<ll>& a, vector<ll>& p, vector<ll>& b, vector<ll>& q) {
+    ll total_cost = 0;
 
-    friend std::ostream& operator<<(std::ostream& os, const modint& rhs) {
-        os << rhs.a;
-        return os;
+    rep(i, n) {
+        ll min_cost_s = LLONG_MAX, min_cost_t = LLONG_MAX;
+        for (ll s = 0; s <= a[i]; s++) {
+            ll t;
+            ll remaining_capacity = max(0LL, W_target - s * a[i]);
+            if(remaining_capacity == 0) t = 0;
+            else t = (remaining_capacity + b[i] - 1) / b[i];
+            ll total = s * p[i] + t * q[i];
+            min_cost_s = min(min_cost_s, total);
+        }
+
+        for(ll t = 0; t <= b[i]; t++){
+            ll s;
+            ll remaining_capacity = max(0LL, W_target - t * b[i]);
+            if(remaining_capacity == 0) s = 0;
+            else s = (remaining_capacity + a[i] - 1) / a[i];
+            ll total = s * p[i] + t * q[i];
+            min_cost_t = min(min_cost_t, total);
+        }
+        total_cost += min(min_cost_s, min_cost_t);
     }
-};
 
-using mint = modint<1000000007>;
+    return total_cost <= x;
+}
 
 int main() {
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
+    cin >> n >> x;
+    vector<ll> a(n), b(n), p(n), q(n);
+    for (ll i = 0; i < n; ++i) {
+        cin >> a[i] >> p[i] >> b[i] >> q[i];
+    }
 
-    rep(i, n){
-        if(i%2 == 1){
-            if(s[i] == 'A') s[i] = 'B';
-            else s[i] = 'A';
+    ll ans = 0;
+    ll ok = -1, ng = 1e9 + 1;
+    while (ng - ok > 1) {
+        ll mid = (ok + ng) / 2;
+        if (taskAchieve(mid, a, p, b, q)) {
+            ans = mid;
+            ok = mid;
+        } else {
+            ng = mid;
         }
     }
-    mint ans = 1;
-    int cnt = 1;
-    rep(i, n-1){
-        if(s[i] == s[i+1]) cnt++;
-        else{
-            ans *= (cnt+1)/2;
-            cnt = 1;
-        }
-    }
-    ans *= (cnt+1)/2;
-    cout << ans << endl;
+
+    cout << ans << endl; 
     return 0;
 }
