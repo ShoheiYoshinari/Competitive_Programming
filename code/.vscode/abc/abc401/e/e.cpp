68a850c54bab75f49a93cabd6c878d92dcd47f12
yoshinari0508 <yoshinari0508@icloud.com>
2025-04-21 17:41:54 +0900

    abc402 5完イイ感じ

--- code/.vscode/abc/abc401/e/e.cpp
+++ code/.vscode/abc/abc401/e/e.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 using ll = long long;
 using ull = unsigned long long;
 const double pi = acos(-1);
@@ -11,17 +13,98 @@ const double pi = acos(-1);
 #define exists(c, e) ((c).find(e) != (c).end())
 template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
 template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+template<class T>constexpr T INF() { return ::std::numeric_limits<T>::max(); }
+template<class T>constexpr T HINF() { return INF<T>() / 2; }
 const int dy[] = {1, 0, -1, 0};
 const int dx[] = {0, 1, 0, -1};
 
 struct INIT{
- INIT(){
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  cout << fixed << setprecision(20);
- }
+INIT(){
+std::ios::sync_with_stdio(false);
+std::cin.tie(0);
+cout << fixed << setprecision(20);
+}
 }INIT;
 
-int main(){
+struct UnionFind {
+    // core member
+    vector<int> par;
+
+    // constructor
+    UnionFind() { }
+    UnionFind(int n) : par(n, -1) { }
+    void init(int n) { par.assign(n, -1); }
     
+    // core methods
+    int root(int x) {
+        if (par[x] < 0) return x;
+        else return par[x] = root(par[x]);
+    }
+    
+    bool same(int x, int y) {
+        return root(x) == root(y);
+    }
+    
+    bool unite(int x, int y) {
+        x = root(x), y = root(y);
+        if (x == y) return false;
+        if (par[x] > par[y]) swap(x, y); // merge technique
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    
+    int size(int x) {
+        return -par[root(x)];
+    }
+    
+    // グループ構造の詳細を取得する関数
+    vector<vector<int>> groups() {
+        // member[v] := 要素 v をリーダーとするグループ
+        vector<vector<int>> member(par.size());
+        for (int v = 0; v < (int)par.size(); ++v) {
+            member[root(v)].push_back(v);
+        }
+        
+        // 配列 member の空の部分を削除したものを作る
+        vector<vector<int>> res;
+        for (int v = 0; v < (int)par.size(); ++v) {
+            if (!member[v].empty()) res.push_back(member[v]);
+        }
+        return res;
+    }
+};
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> G(n);
+    rep(i, m){
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    UnionFind uf(n);
+    vector<bool> red(n);
+    int cnt = 0;
+    rep(i, n){
+        for(auto j : G[i]){
+            if(j < i){
+                uf.unite(i, j);
+            }else{
+                if(red[i]) continue;
+                red[i] = true;
+                cnt++;
+            }
+        }
+        if(red[i]) cnt--;
+
+        int ans = -1;
+        if(uf.size(i) == i+1) ans = cnt;
+        cout << ans << endl;
+    }
+    return 0;
 }
---
+++ code/.vscode/abc/abc402/d/d.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(p) (p).begin(), (p).end()
+#define exists(c, e) ((c).find(e) != (c).end())
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+template<class T>constexpr T INF() { return ::std::numeric_limits<T>::max(); }
+template<class T>constexpr T HINF() { return INF<T>() / 2; }
+const int dy[] = {1, 0, -1, 0};
+const int dx[] = {0, 1, 0, -1};
+
+struct INIT{
+INIT(){
+std::ios::sync_with_stdio(false);
+std::cin.tie(0);
+cout << fixed << setprecision(20);
+}
+}INIT;
+
+bool my_compare(pair<int, int> a, pair<int, int> b) {
+    if(a.first != b.first){
+         return a.first < b.first;
+    }
+    return a.second > b.second;
+}
+
+int main(){
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> a(n, 0);
+    rep(i, m){
+        ll aa, b;
+        cin >> aa >> b;
+        aa--, b--;
+         s = (aa+b)%n;
+        a[s]++;
+    }
+
+    ll ans = m*(m-1)/2;
+    ll noCross = 0;
+    for(ll c : a){
+        noCross += c*(c-1)/2;
+    }
+    cout << ans - noCross << endl;
+    return 0;
+}
